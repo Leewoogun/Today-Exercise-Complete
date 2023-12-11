56085b3e87b5fc4b18252ce132453fbc9e433553
HG <gyrjs0807@gmail.com>
2023-12-11 16:00:47 +0900

    프로젝트 생성 및 MySQL 설정

---
+++ project_server/TodayExercise/src/main/java/com/example/todayexercise/TodayExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.todayexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayExerciseApplication.class, args);
+	}
+
+}
